0f949cc68e56476fd2e8ef911dda8e60e4bdf816
Akiiieee <christianpaulcolo19@gmail.com>
2025-03-23 17:06:40 +0800

    Login Area

---
+++ views/login/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../assets/login.css">
+</head>
+<body>
+    <h1>Login Area</h1>
+    <form action="" class="login" method="POST">
+
+        <input type="text" placeholder="Enter Username or Email">
+        <input type="password" placeholder="Password">
+        <input type="password" placeholder="Confirm Password">
+        <button class="login-bbtn" type="submit"> Login </button>
+        <p>Forgot Password? <button class="reset-password">Reset Password</button></p>
+        <p>Do you have an Account? <button class="sign-up">Sign up</button></p>
+        
+        
+    </form>
+
+</body>
+</html>
