a91d57e88c99342269adc12a587a5a0dfd1d97fd
Russel Jhon <russeljhon.dasigan@evsu.edu.ph>
2025-03-23 13:41:03 +0800

    assets folder updated

--- routes.php
+++ routes.php
@@ -1,5 +1,25 @@
 <?php
 
-  
+  require "controllers/user_controller.php";
+  require "model/user.php";
+  session_start();
+
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $birthdate = $_POST["birthdate"];
+  $birthdate = date("Y-m-d",strtotime(str_replace("/","-", $birthdate)));
+  $username = $_POST["username"];
+  $password = $_POST["password"];
+  $confirmpass = $_POST["confirmpassword"];
+
+  $controller = new UserController($pdo, $key);
+
+
+  if($_SERVER["REQUEST_METHOD"] === "POST"){
+
+    if($_POST["action"] === "register"){
+
+    }
+  }
 
 ?>
