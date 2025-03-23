d7e23945685f5926480bbc5448590b12fee4489c
kenox28 <iquenxzxz@gmail.com>
2025-03-23 14:30:48 +0800

    signup css

--- views/signup/signup.php
+++ views/signup/signup.php
@@ -21,16 +21,34 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link rel="stylesheet" href="../../assets/signup.css">
 </head>
 <body>
   <form action="" class="signup-form">
-    <input type="text" name="name" class="name" required placeholder="Name" autocomplete="name">
-    <input type="date" name="birthdate" class="birthdate" required placeholder="Birthdate" autocomplete="bday">
-    <input type="text" name="email" class="email" required placeholder="Email" autocomplete="email">
-    <input type="text" name="username" class="username" required placeholder="Username" autocomplete="username">
-    <input type="password" name="password" class="password" required placeholder="Password">
-    <input type="password" name="confirmpassword" class="password" required placeholder="Confirm password">
-    <input type="submit" value="Create Account">
+    <div class="container" id="Names">
+      <input type="text" name="fname" id="fname" class="name input" required placeholder="Firstname" autocomplete="name">
+      <input type="text" name="lname" id="lname" class="name input" required placeholder="Surname" autocomplete="name">
+      
+    </div>
+    <div class="container">
+      <input type="text" name="email" class="email input" required placeholder="Email" autocomplete="email">
+    </div>
+    <div class="container">
+      <input type="date" name="birthdate" class="birthdate input" required placeholder="Birthdate" autocomplete="bday">
+    </div>
+
+    <div class="container">
+      <input type="text" name="username" class="username input" required placeholder="Username" autocomplete="username">
+    </div>
+    <div class="container">
+      <input type="password" name="password" class="password input" required placeholder="Password">
+    </div>
+    <div class="container">
+      <input type="password" name="confirmpassword" class="password input" required placeholder="Confirm password">
+    </div>
+    <div class="container">
+      <input type="submit" value="Create Account" class="submitinput">
+    </div>
   </form>
   <script src="../../javascript/login_signup/login_signup.js"></script>
 </body>
