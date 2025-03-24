8ac53e0689c37128ab4c4cd62a13f9d679d758f7
kenox28 <iquenxzxz@gmail.com>
2025-03-24 23:35:12 +0800

    header

--- javascript/post/post.js
+++ javascript/post/post.js
@@ -1,41 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const postForm = document.querySelector(".post-form");
+	const postForm = document.querySelector(".post-form");
 
-  async function create_post(e) {
-    e.preventDefault();
-    const formdata = new FormData(postForm);
+	async function create_post(e) {
+		e.preventDefault();
+		const formdata = new FormData(postForm);
 
-    const response = await fetch("../../routes.php", {
-      method: "POST",
-      body: formdata,
-    });
+		const response = await fetch("../routes.php", {
+			method: "POST",
+			body: formdata,
+		});
 
-    const data = await response.json();
+		const data = await response.json();
 
-    if (data.status === "success") {
-      
-      swal({
-        title: "Success!",
-        text: data.message,
-        icon: "success",
-        button: "OK",
-      });
+		if (data.status === "success") {
+			swal({
+				title: "Success!",
+				text: data.message,
+				icon: "success",
+				button: "OK",
+			});
 
-      console.log(data.status, data.message);
-    } else {
-      swal({
-        title: "Error!",
-        text: data.message,
-        icon: "error",
-        button: "Try Again",
-      });
-      console.log(data.status, data.message);
-    }
-  }
+			console.log(data.status, data.message);
+		} else {
+			swal({
+				title: "Error!",
+				text: data.message,
+				icon: "error",
+				button: "Try Again",
+			});
+			console.log(data.status, data.message);
+		}
+	}
 
-  
-
-  
-  postForm.addEventListener("submit", create_post);
-
+	postForm.addEventListener("submit", create_post);
 });
